52192c4bcf105a689e2006ad43c00ea3bb266d6c
aydanbrown <aydan@evident.co.nz>
2017-03-16 12:16:57 +1100

    Removed old files and folders

--- server/old-handlers/list-questions.js
+++
@@ -1,9 +0,0 @@
-const sql = require('../sql');
-
-module.exports = function(req, res, error) {
-	sql('SELECT * FROM Questions', function(err, result) {
-		if(err) return error(err);
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify(result));
-	});
-}
